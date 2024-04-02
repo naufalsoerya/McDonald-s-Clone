0ba4267263a5afb4af19de1aeccd228c47e05629
naufalsoerya <naufalsurya.official@gmail.com>
2024-04-03 02:25:40 +0700

    updated: navbar fix

--- mcdonald/src/components/NavBar.tsx
+++ mcdonald/src/components/NavBar.tsx
@@ -2,35 +2,22 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <div className="navbar bg-base-100 sticky top-0">
+    <div className="navbar bg-base-100 static top-0">
       <div className="navbar-start px-12 text-yellow-500">
         <Link href={"/home"} className="btn btn-ghost text-xl">McDonald's</Link>
       </div>
-      <div className="navbar-center hidden lg:flex">
+      <div className="navbar-center hidden lg:flex font-bold text-gray-400">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Item 1</a>
+            <a>Menu</a>
           </li>
           <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
+            <a>Wishlist</a>
           </li>
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <a className="btn">Login</a>
       </div>
     </div>
   );
