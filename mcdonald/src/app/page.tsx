d6e2d9f9c50d671f49252d8d01934f5d1980d1c9
naufalsoerya <naufalsurya.official@gmail.com>
2024-04-02 20:43:44 +0700

    updated: folder file

--- mcdonald/src/app/page.tsx
+++ mcdonald/src/app/page.tsx
@@ -1,47 +1,33 @@
+import NavBar from "@/components/NavBar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <div className="navbar bg-base-100">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      {/* Component NavBar */}
+      <NavBar />
+
+      {/* Carousel */}
+      <div className="carousel w-full h-64 carousel-center box">
+        <div className="carousel-item">
+          <img
+            src="https://d2vuyvo9qdtgo9.cloudfront.net/promos/June2022/jR9u1QhFGku4ru60QhN5.jpg"
+            alt="Promo-1"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://d2vuyvo9qdtgo9.cloudfront.net/promos/August2022/QIUEnOkYq1zTG354ZaqS.jpg"
+            alt="Promo-2"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://d2vuyvo9qdtgo9.cloudfront.net/promos/August2023/wff6zUYmkHwxsOw4lfoS.png"
+            alt="Pizza"
+          />
+        </div>
       </div>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-</div>
     </>
   );
 }
